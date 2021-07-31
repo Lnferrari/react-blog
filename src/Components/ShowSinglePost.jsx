8c47779fd9309ec57c73f7910ff3c25f8c0f3276
Pedro Vazquez <vazquezpedronicolas@gmail.com>
2021-07-31 11:01:29 -0300

    acomodanding

--- src/Components/ShowSinglePost.jsx
+++ src/Components/ShowSinglePost.jsx
@@ -1,29 +1,28 @@
 import React from 'react'
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 function ShowSinglePost({posts}) {
-    const {postID} = useParams()
+    const { postID } = useParams()
 
-    const singlePost = postID && (posts.map((item) => {
-        return item.id === postID ? (
-            <div className='Post' key={item.id}>
-                <h2>{item.title}</h2>
-                <p>{item.content}</p>
-                <p className='footer italic'>{item.date} @ {item.username}</p>
-            </div>
-        ) : null
+    const singlePost = postID && (posts.map(item => {
+      return item.id === postID && (
+        <div className='Post' key={item.id}>
+          <h2>{item.title}</h2>
+          <p>{item.content}</p>
+          <p className='footer italic'>{item.date} @ {item.username}</p>
+        </div>
+      )
     }))
+    
     return (
-        <>
-            <h1>Post</h1>
-            <div className="container">
-                <div className="frameA">
-                    <div className='frameB'>
-                        {singlePost}
-                    </div>
-                </div>
-            </div>
-        </>
+      <>
+        <h1>Post</h1>
+        <div className="container">
+          <div className="frameA">
+            <div className='frameB'>{singlePost}</div>
+          </div>
+        </div>
+      </>
     )
 }
 
